ce19c925ea3402b33fe05243a8cc3d73b23dee9c
UsmanHashimov <hashimovusman01@gmail.com>
2024-04-16 09:45:50 +0500

    material ui done :+1:

---
+++ Material-UI-Frontend/src/app/components/getall/getall.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../services/crud.service';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-getall',
+  templateUrl: './getall.component.html',
+  styleUrl: './getall.component.css'
+})
+export class GetallComponent implements OnInit {
+
+  displayedColumns: string[] = ['name', 'email', 'id', 'ticketid'];
+
+  users! : User[]
+
+  constructor(private crudService: CrudService) { }
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.crudService.getAll().subscribe({
+      next: (data) => {
+        this.users = data;
+
+        console.log(data);
+      },
+
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
+}
